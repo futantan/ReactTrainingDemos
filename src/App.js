1f98ae05698103ee7c81693f6bf1a42772a930fe
Tantan Fu <ttfu@ttfu.lan>
2017-07-12 22:16:23 +0800

    Add fakeData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+const fakeData = {
+  items: [
+    { content: 'hello', status: 'completed' },
+    { content: 'world', status: 'active' },
+    { content: 'how', status: 'active' },
+    { content: 'are', status: 'active' },
+    { content: 'you', status: 'active' },
+  ],
+  filter: 'all',
+};
+
 class App extends Component {
   render() {
     return (
-      <div>todo</div>
+      <div>
+        todo
+      </div>
     );
   }
 }
