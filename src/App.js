2da5d30bb7ff9628db218af963d20d51990fbe5a
Tantan Fu <ttfu@ttfu.local>
2017-07-13 09:56:49 +0800

    Refactor pure html to static component

--- src/App.js
+++ src/App.js
@@ -11,26 +11,39 @@ const fakeData = {
   filter: 'all',
 };
 
+const TextFieldSubmit = (props) => (
+  <form>
+    <input type="text"/>
+    <button type="submit">Add Todo</button>
+  </form>
+);
+
+const List = (props) => (
+  <ul>
+    {props.items.map((item) => <Item content={item.content}/>)}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>{props.content}</li>
+);
+
+const Filter = (props) => (
+  <div>
+    <span>Show:</span>
+    <a href="">All</a>
+    <a href="">Active</a>
+    <a href="">Completed</a>
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
       <div>
-        <form>
-          <input type="text"/>
-          <button type="submit">Add Todo</button>
-        </form>
-
-        <ul>
-          <li>foo</li>
-          <li>bar</li>
-        </ul>
-
-        <div>
-          <span>Show:</span>
-          <a href="">All</a>
-          <a href="">Active</a>
-          <a href="">Completed</a>
-        </div>
+        <TextFieldSubmit/>
+        <List items={fakeData.items}/>
+        <Filter/>
       </div>
     );
   }
