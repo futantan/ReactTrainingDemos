494494545c4d3ef8057bc00f32aa71bcefce60b9
futantan <luckytantanfu@gmail.com>
2017-07-13 14:33:42 +0800

    Refactor extract method

--- src/App.js
+++ src/App.js
@@ -102,35 +102,40 @@ class App extends Component {
     todos: [],
   };
 
+  handleAddTodoItem = (content) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({ content, status: 'active' })
+    });
+  };
+
+  handleToggleTodo = (index) => {
+    const targetTodo = this.state.todos[index];
+    const toggledStatus = (targetTodo.status === 'active' ? 'completed' : 'active');
+
+    this.setState({
+      ...this.state,
+      todos: [
+        ...this.state.todos.slice(0, index),
+        { ...targetTodo, status: toggledStatus },
+        ...this.state.todos.slice(index + 1)
+      ],
+    });
+  };
+
+  todosWithCondition = (condition) => {
+    return this.state.todos
+      .filter((item) => (condition === 'all') ? true : item.status === condition);
+  };
+
   render() {
     return (
       <div>
-        <TextFieldSubmit addTodoItem={(content) => {
-          this.setState({
-            ...this.state,
-            todos: this.state.todos.concat({ content, status: 'active' })
-          });
-        }}/>
+        <TextFieldSubmit addTodoItem={this.handleAddTodoItem}/>
 
         <List
-          items={
-            this.state.todos
-              .filter((item) => (this.state.filter === 'all') ? true : item.status === this.state.filter)
-          }
-          toggleTodo={(index) => {
-            const targetTodo = this.state.todos[index];
-            const toggledStatus = (targetTodo.status === 'active' ? 'completed' : 'active');
-
-            this.setState({
-              ...this.state,
-              todos: [
-                ...this.state.todos.slice(0, index),
-                {...targetTodo, status: toggledStatus },
-                ...this.state.todos.slice(index + 1)
-              ],
-            });
-          }}
-
+          items={this.todosWithCondition(this.state.filter)}
+          toggleTodo={this.handleToggleTodo}
         />
 
         <FilterContainer
